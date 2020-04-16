5d916f7fdfe7a57c561030b4914faf58e1ce4c90
(lopintasarungallo) <lopinsarungallo@gmail.com>
2020-04-16 21:42:57 +0700

    Tugas 5

---
+++ H071191025/Tugas-5/Converter.java
@@ -0,0 +1,19 @@
+/**
+
+ * Converter
+
+ */
+
+public abstract class Converter {
+
+
+
+    public abstract void start();
+
+    public abstract void stop();
+
+    
+
+
+
+}
---
+++ H071191025/Tugas-5/Temperature.java
@@ -0,0 +1,17 @@
+/**
+
+ * Temperature
+
+ */
+
+public interface Temperature {
+
+
+
+    public abstract double celciusToFahrnheit(double value);
+
+    public abstract double celciusToKelvin(double value);
+
+
+
+}
